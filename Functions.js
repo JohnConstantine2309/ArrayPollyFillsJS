251cbd84ab4899603c32af40437f6c1d60687ded
JohnConstantine2309 <159234107+JohnConstantine2309@users.noreply.github.com>
2025-11-18 10:13:04 +0530

    Create Functions.js

---
+++ Functions.js
@@ -0,0 +1,48 @@
+function currying(fn) {
+    return function curried(...args) {
+        if(args.length >= fn.length) {
+            return fn.apply(this, args)
+        }
+        else {
+            return function(...nextArgs) {
+                return curried(...args, ...nextArgs)
+            }
+        }
+    }
+}
+
+function debounce(fn, delay) {
+    let interval;
+    
+    return function(...args) {
+        clearTimeout(interval)
+        interval = setTimeout(() => {
+            fn.apply(this, args)
+        }, delay)
+    }
+}
+
+function throttle(fn, delay) {
+    let last = 0
+    return function(...args) {
+        let now = Date.now();
+        if(now-last >=delay) {
+            last = now
+            fn.apply(this, args)
+        }
+    }
+}
+
+function memoise(fn) {
+    let cache = new Map();
+    return function(...args) {
+        let key = JSON.stringify(args)
+        if(cache.has(key)) {
+            return cache.get(key)
+        }
+        
+        let result = fn.apply(this, args)
+        cache.set(key, result)
+        return result
+    }
+}
